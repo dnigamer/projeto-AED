5147c20aea8ad6e66a52b9ef515cd1a8608dd51d
Gonçalo M. <dnigamerofficial@gmail.com>
2024-04-21 18:16:23 +0100

    Removido iohelp.c. Fix iohelp.h.

--- src/iohelp.h
+++ src/iohelp.h
@@ -6,10 +6,43 @@
 #else                                  // Linux or macOS
 #include <termios.h>
 static struct termios old, current;
-void initTermios(int echo);
-void resetTermios(void);
-char getch();
-char getche();
+
+void initTermios(int echo) {
+    tcgetattr(0, &old);
+    current = old;
+    current.c_lflag &= ~ICANON;
+    switch (echo) {
+        case 1:
+            current.c_lflag |= ECHO;
+            break;
+        case 0:
+            current.c_lflag &= ~ECHO;
+            break;
+        default:
+            break;
+    }
+    tcsetattr(0, TCSANOW, &current);
+}
+
+void resetTermios(void) {
+    tcsetattr(0, TCSANOW, &old);
+}
+
+char getch() {
+    char ch;
+    initTermios(0);
+    ch = (char) getchar();
+    resetTermios();
+    return ch;
+}
+
+char getche() {
+    char ch;
+    initTermios(1);
+    ch = (char) getchar();
+    resetTermios();
+    return ch;
+}
 #endif
 
 #endif
